53070a06c66a0bbb1cc85ec426a90488908b88e3
bb2468 <55417058+bb2468@users.noreply.github.com>
2021-11-30 20:35:24 -0600

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -26,21 +26,21 @@ ShoppingCart::~ShoppingCart()
 void ShoppingCart::add(item)//adds item to cart
 {
 
-  // creates the json dictionary
-	Json::Value stock_json;
+ // creates the json dictionary
+Json::Value stock_json;	
+// loads data from file
+std::ifstream stockfile ("./inventory.json");
+stockfile >> stock_json;
+stockfile.close();
 	
-	// loads data from file
-	std::ifstream stockfile ("./inventory.json");
-	stockfile >> stock_json;
-	stockfile.close();
-	
-	for (int i = 0; i<stock_json["name"]; i++) {
-		if (stock_json["names"][i] == item.get_name())
-			// this code will run if the item is in stock!
-	}
-  
-if(
- incart.push_back(item)
+for (int i = 0; i<stock_json["name"]; i++) 
+{
+	if (stock_json["names"][i] == item.get_name())// this code will run if the item is in stock. If the item exists, there is at least one
+	{
+		incart.push_back(item)
+	}		
+	} 
+ 
 }
 
 void ShoppingCart::remove(item)//removes an instance of an item from cart
