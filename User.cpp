a52a8d69c3fc062461358123d7a33ac356ceb9c6
jk1834 <95237166+jk1834@users.noreply.github.com>
2021-11-30 20:11:02 -0600

    Update User.cpp

--- User.cpp
+++ User.cpp
@@ -39,7 +39,26 @@ void User::set_payment_info(int cardNum)
 
 void User::create_account(string name, string password, int cardNum, string address)
 {
+    Json::Value user;
     
+    for(int i = 0;i<user["username"].size(); i++)
+    {
+        if(user["username"][i] == name)
+        {
+            cout << "There is already a user with this name." << endl;
+            return;
+        }
+    }
+    
+    user["username"].append(name);
+    user["password"].append(password);
+    user["paymentinfo"].append(cardNum);
+    user["address"].append(address);
+    
+    std::ofstream outuserfile ("./user.json");
+    outuserfile << user;
+    outuserfile.close;
+        
 }
 
 
