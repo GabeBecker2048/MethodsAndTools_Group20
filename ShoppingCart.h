384eb292eda1c1701fc368216756fe156c5b95b1
bb2468 <55417058+bb2468@users.noreply.github.com>
2021-11-30 19:25:31 -0600

    Update ShoppingCart.h

--- ShoppingCart.h
+++ ShoppingCart.h
@@ -1,1 +1,30 @@
-//shopping cart
+//shopping cart h file
+#include <string>
+#include <vector>
+
+#include "item.h"
+#include "inventory.h"
+
+#ifndef SHOPPINGCART_H
+#define SHOPPINGCART_H
+
+using std::string;
+using std::vector;
+
+
+
+Class ShoppingCart
+{
+
+private:
+
+
+public:
+
+
+
+
+};
+
+
+#endif
