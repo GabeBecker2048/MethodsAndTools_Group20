bc0cd31256f051726b84fb22a9f05acb99c33ca8
jk1834 <95237166+jk1834@users.noreply.github.com>
2021-11-30 19:31:43 -0600

    Add files via upload

---
+++ User.cpp
@@ -0,0 +1,58 @@
+#include "User.h"
+
+
+
+
+void User::login(string name, string password)
+{
+    
+}
+
+
+void User::logout()
+{
+    
+}
+
+
+string User::get_shipping_info()
+{
+    return address;
+}
+
+
+void User::set_shipping_info(string address)
+{
+    this->address = address;
+}
+
+
+int User::get_payment_info()
+{
+    return cardNum;
+}
+
+
+void User::set_payment_info(int cardNum)
+{
+    this->cardNum = cardNum;
+}
+
+
+void User::create_account(string name, string password)
+{
+    
+}
+
+
+void User::delete_account()
+{
+    
+}
+
+
+void User::checkout()
+{
+    
+}
+
