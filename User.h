e7e6cbd657a80a3c413eac8af78871ff9f187853
jk1834 <95237166+jk1834@users.noreply.github.com>
2021-11-30 19:31:18 -0600

    Add files via upload

---
+++ User.h
@@ -0,0 +1,44 @@
+#pragma once
+#include <string>
+#include <iostream>
+#include <fstream>
+
+#include "ShoppingCart.h"
+
+using std::string;
+
+
+class User {
+    private:
+        
+        ShoppingCart cart;
+        string name;
+        string password;
+        int cardNum;
+        string address;
+    
+    public:
+        
+        User() : name(" "), password(" ") {}
+        ~User();
+        
+        void login(string name, string password);
+        void logout();
+        
+        string get_shipping_info();
+        void set_shipping_info(string address);
+        
+        int get_payment_info();
+        void set_payment_info(int cardNum);
+        
+        void create_account(string name, string password);
+        void delete_account();
+        
+        void checkout();
+};
+
+
+
+
+
+
