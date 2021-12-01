f2c057a3e48d14450bf75294bac3394052f3084f
jrow2018 <44179491+jrow2018@users.noreply.github.com>
2021-11-30 20:25:40 -0600

    Add files via upload

--- item.cpp
+++ item.cpp
@@ -5,6 +5,23 @@
 using std::string;
 
 string Item::get_name() {
-return name;}
+  return name;
+}
+
 string Item::get_category() {
-return category;}
+  return category;
+}
+
+void Item::set_price(double p) {
+  this -> price = p;
+}
+
+double Item::get_price() {
+  return price;
+}
+
+Item::Item(string x, string y, double z) {
+  name = x;
+  category = y;
+  price = z;
+}
--- item.h
+++ item.h
@@ -8,25 +8,13 @@ private:
   double price;
 
 public:
-  Item(string x, string y, double z) {
-    name = x;
-    category = y;
-    price = z;
-  }
+  Item(string x, string y, double z);
 
-  string get_name() {
-    return name;
-  }
+  string get_name();
 
-  string get_category() {
-    return category;
-  }
+  string get_category();
 
-  void set_price(double p) {
-    this -> price = p;
-  }
+  void set_price(double p);
 
-  double get_price() {
-    return price;
-  }
+  double get_price();
 };
